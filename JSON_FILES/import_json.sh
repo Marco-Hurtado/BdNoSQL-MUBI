60800d64613a6c27fcc175da6c2fc4385b7e3a3c
TheMisa98 <redomy9912@gmail.com>
2024-06-05 21:13:27 -0600

    load cypher and mongo

---
+++ JSON_FILES/import_json.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+# Configuración de la URI de MongoDB
+mongo_uri="mongodb://localhost:27017"
+
+# Nombre de la base de datos y colección
+db_name="MUBI"
+collection_name="resenias"
+
+# Directorio donde se encuentran los archivos JSON (asegúrate de ajustarlo a tu estructura de directorios)
+json_dir="/home/misa/Documentos/CienciDeDatos/6TO SEMESTRE/BD_noSQL/BdNoSQL-MUBI/JSON_FILES/resenias/"
+
+# Iterar sobre los archivos del 01 al 20
+for i in {0000..0145}; do
+    # Nombre del archivo JSON
+    json_file="resenias${i}.json"
+
+    # Ruta completa al archivo JSON
+    file_path="${json_dir}${json_file}"
+
+    # Verificar si el archivo existe antes de importarlo
+    if [ -f "$file_path" ]; then
+        # Ejecutar mongoimport para el archivo actual
+        echo "Importando archivo ${json_file}..."
+        mongoimport --uri "${mongo_uri}" --db "${db_name}" --collection "${collection_name}" --file "${file_path}"
+        echo "Archivo ${json_file} importado correctamente."
+    else
+        echo "El archivo ${json_file} no existe en la ruta especificada."
+    fi
+done
